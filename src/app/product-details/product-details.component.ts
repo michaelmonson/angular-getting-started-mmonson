756eddbb6c7c69f186d528149c6b046787fdf5e6
michaelmonson <mmonson2000@gmail.com>
2022-03-19 08:15:04 -0600

    Enhance and view product details

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    //Glean the product ID from the current route:
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    //Find the product that matches its ID based on the current route:
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
 
 }
